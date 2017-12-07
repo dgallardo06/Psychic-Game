1ed7a808f838132e6b3b347dac9260ab40eb7d8a
dgallardo06 <denise.gallardo1@gmail.com>
2017-12-06 19:21:35 -0500

    logic for tracking wins, losses, guesses left, letters and reset

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+
+var guesses = "";
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+document.onkeyup = function(event){
+
+	//Determines which key is pressed.
+	var playerGuess = event.key;
+
+	//Radomly chooses letter
+	var letter = letters[Math.floor(Math.random() * letters.length)];
+
+	//Determines if player guessed right letter.
+	//Player gets 10 guesses. After 10 guesses, player loses.
+	for (i = 1; i > 0; i--){
+		guessesLeft--;
+		guesses = playerGuess + " " + guesses;
+
+		if (playerGuess === letter){
+			wins++;
+		} else if (guessesLeft === 0) {
+			losses++;
+			guessesLeft = 10;
+			guesses = "";
+		}
+	
+	}
+
+	//wins, losses, guesses left and letters guessed gets updated
+	var winner = document.getElementById("wins");
+	winner.innerHTML = wins;
+
+	var loser = document.getElementById("losses");
+	loser.innerHTML = losses;
+
+	var remaining = document.getElementById("guessesLeft");
+	remaining.innerHTML = guessesLeft;
+
+	var guessed = document.getElementById("guesses");
+	guessed.innerHTML = guesses;
+}
+
+
+
